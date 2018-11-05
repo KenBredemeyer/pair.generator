8576e1b5c505815bae9dea0ed23df1ce011f026e
Ken Bredemeyer <ken.bredemeyer@uwa.edu.au>
2018-11-05 15:43:52 +0800

    add examples to documentation.

--- R/plot_funs.R
+++ R/plot_funs.R
@@ -1,6 +1,18 @@
-#' barchart to see how well each script is represented.
+#' Barchart to see how well each performance is represented.
+#'
+#' To test if \code{pairs_generate} produces pairs which conform to the
+#' specified range of inclusions of performances, use
+#' \code{pairs_plot_inclusions} together with \code{abline} (see the example
+#' below), or use \code{\link{pairs_inclusions_range}}.
 #'
 #' @param pairs A data frame containing pairs of performances.
+#' @examples
+#' pairs <- pairs_generate(letters[1:10],
+#'                          av_inclusions = 10,
+#'                          inclusion_tolerance = 2,
+#'                          chain_length = 1)
+#' pairs_plot_inclusions(pairs)
+#' abline(h = c(10-2, 10+2))
 #' @export
 pairs_plot_inclusions <- function(pairs) {
 	sampling <- rle(sort(unlist(pairs[ , 1:2])))
@@ -17,7 +29,18 @@ pairs_plot_inclusions <- function(pairs) {
 }
 
 #' Range of inclusions per media/performance
-#' @param pairs data.frame returned from \code{pairs_generate}
+#' @param pairs data.frame returned from \code{pairs_generate},
+#'   \code{pairs_generate_cnc}, or similar function.
+#'
+#' @return Numeric vector length 2, reporting the minimum and maximum number of
+#'   inclusions per performance in the set of pairs used as its first argument.
+#'
+#' @examples
+#' pairs <- pairs_generate(letters[1:3],
+#'                         av_inclusions = 2,
+#'	                       inclusion_tolerance = 1,
+#'	                       chain_length = 1)
+#' pairs_inclusions_range(pairs)
 #' @export
 pairs_inclusions_range <- function(pairs) {
 	sampling <- rle(sort(unlist(pairs[ , 1:2])))
@@ -26,10 +49,17 @@ pairs_inclusions_range <- function(pairs) {
 	range(s.f.)
 }
 
-#' Plot the frequency of pairs
+#' Plot the frequency of unique pairs included in \code{pairs}
 #'
 #' @param pairs data.frame returned from \code{pairs_generate} or similar
 #'   function.
+#' @seealso \code{\link{pairs_plot_inclusions}}
+#' @examples
+#' gp <- pairs_generate(letters[1:7],
+#'	                     av_inclusions = 15,
+#'	                     inclusion_tolerance = 2,
+#'	                     chain_length = 1)
+#' pairs_plot(gp)
 #' @export
 pairs_plot <- function(pairs) {
 	# recover script labels
@@ -62,18 +92,27 @@ pairs_plot <- function(pairs) {
 
 #' Plot the scores of performances in each pair
 #'
-#' This plot can be used to check that groups of performances are not isolate, and
-#' provides an indication that the separation constraint used in
-#' \code{pairs_generate} or similar function is working correctly.
+#' This plot can be used to check that groups of performances are not isolate,
+#' and provides an indication that the separation constraint used in
+#' \code{pairs_generate} or similar function is working correctly.  Currently,
+#' overplotting of performance labels occurs for performances which have close
+#' scores.
 #'
-#' @param pairs Data frame returned from \code{pairs_generate} or similar function.
-#' @param scores_df Data frame containing the variables \code{media, core, score}
+#' @param pairs Data frame returned from \code{pairs_generate} or similar
+#'   function.
+#' @param scores_df Data frame containing the variables \code{media, core,
+#'   score}
+#' @examples
+#' pairs <- pairs_generate(data_standard[1:10, ],
+#'	                      av_inclusions = 10,
+#'	                      inclusion_tolerance = 2, chain_length = 1)
+#' pairs_plot_scores(pairs, data_standard[1:10, ])
 #' @export
 pairs_plot_scores <- function(pairs, scores_df) {
   pairs_table <- merge(pairs, scores_df, by.x = "left", by.y = "media", sort = FALSE)
   pairs_table <- merge(pairs_table, scores_df, by.x = "right", by.y = "media", sort = FALSE)
 	graphics::plot(-1, -1, xlim = c(0, 1.1), ylim = c(0, max(c(pairs_table$score.x, pairs_table$score.y))), xlab = "", ylab = "score", xaxt = "na",
-	     main = "Paired Comparisons \n showing locations of pairings",
+	     main = "Scores of Performances in Each Pair",
 	     sub = "Pairings")
 	graphics::axis(side = 1, at = 0:1, labels = c("left", "right"))
 	for(i in 1:nrow(pairs_table)) {
