0f8d47db19cffd540d28e0dff6431235b12d85f1
Ken Bredemeyer <ken.bredemeyer@uwa.edu.au>
2018-11-06 15:26:11 +0800

    add example to score_diff() documentation.

--- R/check_pairs.R
+++ R/check_pairs.R
@@ -1,13 +1,26 @@
-#' Show scores and score difference for a set of pairs
+#'Show scores and score difference for a set of pairs
 #'
-#' @param pairs data.frame returned from \code{generate_pairs} or similar
-#'   function
-#' @param media data.frame containing the variables \code{media, core, score}.
-#'   \code{score} must be numeric.
+#'Use this function to check that a separation constraint has been successfully
+#'applied using \code{pairs_generate}, or similar function.
 #'
-#' @return data.frame similar to \code{pairs}, but with \code{left_score,
-#'   right_score, score_difference} added.
-#' @export
+#'@param pairs data.frame returned from \code{generate_pairs} or similar
+#'  function
+#'@param media data.frame containing the variables \code{media, core, score}.
+#'  \code{score} must be numeric.
+#'
+#'@return data.frame similar to \code{pairs}, but with \code{left_score},
+#'  \code{right_score}, and \code{score_difference} columns added.
+#' @examples
+#' # check separation constraint
+#' pairs <- pairs_generate(data_standard,
+#'	                        av_inclusions = 5,
+#'	                        inclusion_tolerance = 2,
+#'	                        chain_length = 1,
+#'	                        separation_constraint = 30)
+#' pairs_scores <- score_diff(gpp, data_standard)
+#' max(pairs_scores$score_difference)
+#'
+#'@export
 score_diff <- function(pairs, media) {
 	stopifnot(class(pairs) == "data.frame", class(media) == "data.frame")
 	stopifnot(!is.null(pairs$left), !is.null(pairs$right))
