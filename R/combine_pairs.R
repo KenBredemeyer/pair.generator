3583a34b6ad1f28129e8b53231e5f9275b70ffc1
Ken Bredemeyer <ken.bredemeyer@uwa.edu.au>
2019-05-30 13:10:50 +0800

    Icrease chain number of 2nd arg if needed.  All chain numbers should be unique.

--- R/combine_pairs.R
+++ R/combine_pairs.R
@@ -1,8 +1,8 @@
 #' Combine two sets of pairs into one data frame, randomising the chains
 #'
-#' For better interoperability with 'Pairwise Comparisons Application' please
-#' ensure that \code{pairs1$chain_number} is different to
-#' \code{pairs2$chain_number}
+#' If \code{pairs1} and \code{pairs2} have any chain numbers in common,
+#' \code{pairs2$chain_number} will be increased, so that chain numbers are
+#' unique.
 #'
 #' @param pairs1 data frame containing the variable \code{chain_number}
 #' @param pairs2 data frame containing the variable \code{chain_number}
@@ -15,7 +15,7 @@ combine_pairs <- function(pairs1, pairs2) {
 		colnames(pairs1) == colnames(pairs2),
 		!is.null(pairs1$chain_number), !is.null(pairs2$chain_number))
 	if(length(intersect(pairs1$chain_number, pairs2$chain_number)) != 0) {
-		pairs2$chain_number <- paste0(pairs2$chain_number, "_set2")
+		pairs2$chain_number <- as.numeric(pairs2$chain_number) + max(as.numeric(pairs1$chain_number))
 	}
 	randomised_chains <- sample(c(unique(pairs1$chain_number), unique(pairs2$chain_number)))
 	combined_df <- rbind(pairs1, pairs2)
@@ -26,9 +26,6 @@ combine_pairs <- function(pairs1, pairs2) {
 	}
 	row_index <- do.call(c, row_indices)
 	combined_df <- combined_df[row_index, ]
-	strings <- strsplit(combined_df$chain_number, "_")
-	if(any(unlist(strings) == "set2")) {
-		combined_df$chain_number <- as.numeric(do.call(rbind, strings)[ , 1])
-	}
+
 	combined_df
 }
